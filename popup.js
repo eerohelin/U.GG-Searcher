c6cc2a6c030898c7fa926d6ba8aeb916898e0b1f
Eero Helin <eero.helin04@gmail.com>
2022-03-05 03:55:28 +0200

    Added styling and QoL changes + preparations for settings

--- popup.js
+++ popup.js
@@ -1,11 +1,15 @@
 var championName = document.getElementById("textInput");
 var searchButton = document.getElementById("searchButton");
+var selectedRank = document.getElementById("rank");
+
 var defaultlink = "https://u.gg/lol/champions/tryndamere/build?rank=master_plus";
 var link1 = "https://u.gg/lol/champions/";
 var link2 = "/build?rank=";
-var rank = "master_plus";
+
+
 
 searchButton.onclick = function() {
+    var rank = selectedRank.value;
     var champion = championName.value;
     var newlink =  link1 + champion + link2 + rank;
     chrome.tabs.update({active: true, url: newlink});
