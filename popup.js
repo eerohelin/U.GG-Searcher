1b60849dfdbb23c6eac56bf3095c71f016aed19b
Eero Helin <eero.helin04@gmail.com>
2022-03-05 04:28:38 +0200

    Added comments

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// Get inputs
 var championName = document.getElementById("textInput");
 var searchButton = document.getElementById("searchButton");
 var selectedRank = document.getElementById("rank");
@@ -6,11 +7,15 @@ var defaultlink = "https://u.gg/lol/champions/tryndamere/build?rank=master_plus"
 var link1 = "https://u.gg/lol/champions/";
 var link2 = "/build?rank=";
 
-
-
+// Search function
 searchButton.onclick = function() {
+
+    // Creating the link
     var rank = selectedRank.value;
     var champion = championName.value;
     var newlink =  link1 + champion + link2 + rank;
+
+    // Opening the link and closing the extension
     chrome.tabs.update({active: true, url: newlink});
+    window.close();
 }
